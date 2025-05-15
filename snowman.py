95acbca8dc02c58bce469f6d1d3925b07b48a89f
lunan0va <schnepf.luna@gmail.com>
2025-05-15 13:21:18 +0200

    Add replay option after each game

--- snowman.py
+++ snowman.py
@@ -1,4 +1,9 @@
 from game_logic import play_game
 
 if __name__ == "__main__":
-    play_game()
+    while True:
+        play_game()
+        again = input("Play again? (y/n): ").lower()
+        if again != "y":
+            print("Thanks for playing Snowman Meltdown!")
+            break
